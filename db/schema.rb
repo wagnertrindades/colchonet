f40feb7b9541c8b5e73431e212a296f8350adc07
Wagner Trindade <wtrindades@hotmail.com>
2016-03-07 20:02:20 -0300

    Capitulo 5

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160306215607) do
+ActiveRecord::Schema.define(version: 20160306221113) do
 
   create_table "rooms", force: :cascade do |t|
     t.string   "title"
@@ -19,6 +19,16 @@ ActiveRecord::Schema.define(version: 20160306215607) do
     t.text     "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "full_name"
+    t.string   "email"
+    t.string   "password"
+    t.string   "location"
+    t.text     "bio"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
