50a685570cfedb46178d20ee5f8391a3b03dd30b
Wagner Trindade <wtrindades@hotmail.com>
2016-03-12 12:10:02 -0300

    Add cryptografy in user password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,9 @@
 class User < ActiveRecord::Base
 
-  validates_presence_of :email, :full_name, :location, :password
-  validates_confirmation_of :password
+  validates_presence_of :email, :full_name, :location
   validates_length_of :bio, :minimum => 30, :allow_blank => false
   validates_format_of :email, :with => /\A[^@]+@([^@\.]+\.)+[^@\.]+\z/
   validates_uniqueness_of :email
+
+  has_secure_password
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160306221113) do
+ActiveRecord::Schema.define(version: 20160312150331) do
 
   create_table "rooms", force: :cascade do |t|
     t.string   "title"
@@ -24,11 +24,11 @@ ActiveRecord::Schema.define(version: 20160306221113) do
   create_table "users", force: :cascade do |t|
     t.string   "full_name"
     t.string   "email"
-    t.string   "password"
+    t.string   "password_digest"
     t.string   "location"
     t.text     "bio"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
 end
