c2d14ec5d1daab222d5fabf346a826493d02492f
Wagner Trindade <wtrindades@hotmail.com>
2016-03-13 17:00:37 -0300

    Capitulo 8

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
-  resources :rooms
-  resources :users
+  LOCALES = /en|pt\-BR/
 
+  scope "(:locale)", :locale => LOCALES do
+    resources :rooms
+    resources :users
+
+    resource :user_confirmation, :only => [:show]
+  end
+
+  match '/:locale' => 'home#index', :locale => LOCALES, via: [:get]
   root :to => "home#index"
 end
