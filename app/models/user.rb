44b1b1da8757046a4c547beac0dc989e4ccc0d29
Wagner Trindade <wtrindades@hotmail.com>
2016-03-20 15:44:09 -0300

    Capitulo 10

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,10 @@
 class User < ActiveRecord::Base
+  # Scopes examples
+  #scope :most_recent, -> { order('created_at DESC') }
+  #scope :from_sampa, -> { where(:location => 'São Paulo') }
+
+  # scope :from_location, ->(location) { where(:location => location) }
+  scope :confirmed, -> { where('confirmed_at IS NOT NULL') }
 
   validates_presence_of :email, :full_name, :location
   validates_length_of :bio, :minimum => 30, :allow_blank => false
@@ -8,6 +14,17 @@ class User < ActiveRecord::Base
   has_secure_password
 
   before_create :generate_token
+
+  # Default Scope
+  # def self.default_scope
+  #    where('confirmed_at IS NOT NULL')
+  # end
+
+  def self.authenticate(email, password)
+    confirmed
+      .find_by_email(email)
+      .try(:authenticate, password)
+  end
 
   def generate_token
     self.confirmation_token = SecureRandom.urlsafe_base64
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
     resources :users
 
     resource :user_confirmation, :only => [:show]
+    resource :user_sessions, :only => [:create, :new, :destroy]
   end
 
   match '/:locale' => 'home#index', :locale => LOCALES, via: [:get]
